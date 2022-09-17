6f8f8c6cb2a637f22b7665091464bbba05d66309
Siddharth Singh <ssiddharth@athenahealth.com>
2022-09-17 14:09:08 +0530

    Solution to problem remove elements

---
+++ java/RemoveElement.java
@@ -0,0 +1,24 @@
+//Solution to problem https://leetcode.com/problems/remove-element/
+package java;
+
+public class RemoveElement {
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int j=0;
+            for(int i=0;i<nums.length;i++)
+            {
+                if(nums[i]!=val)
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                    j++;
+                    
+                }
+            }
+            return j;
+            
+        }
+    }
+    
+}
