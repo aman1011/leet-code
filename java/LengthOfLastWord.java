62c59f7074569ddf8671d72a5ac92271a5e5ca00
Siddharth Singh <ssiddharth@athenahealth.com>
2022-09-18 14:29:46 +0530

    Solution for question https://leetcode.com/problems/length-of-last-word/

---
+++ java/LengthOfLastWord.java
@@ -0,0 +1,30 @@
+//Solution for question https://leetcode.com/problems/length-of-last-word/
+package java;
+
+public class LengthOfLastWord {
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            if(s.length()==1)
+                return 1;
+            String a="";
+            for(int i=s.length()-1;i>=0;i--) {
+                if(s.charAt(i)!=' ')
+                  a+=s.charAt(i);
+                if(s.charAt(i)!= ' ' && i==0)
+                {
+                    break;
+                }
+            
+                if(s.charAt(i)!=' ' &&s.charAt(i-1)==' ')
+                   {
+                    
+                    break;
+                }
+                
+            }
+           
+            return a.length();
+        }
+    }
+    
+}
