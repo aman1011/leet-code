4a47b1b4756550fcd65da5a3fcfcae128b312e49
Siddharth Singh <ssiddharth@athenahealth.com>
2022-10-09 13:59:05 +0530

    Faster solution for inrder traversal

--- java/InorderTraversal.java
+++ java/InorderTraversal.java
@@ -30,3 +30,25 @@ class Solution {
         inorder(root.right);
     }
 }
+
+//Faster solution
+
+
+class Solution {
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null)
+            return list;
+        inorder(root,list);
+        return list;
+    
+    }
+    public void inorder(TreeNode root,List<Integer> list) {
+        if(root==null)
+            return;
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+}
